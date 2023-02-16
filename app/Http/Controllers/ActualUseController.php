8884fa4200bf12cf95aa38432234b0904ade1bf3
badsektor <gerald.villaran@lspu.edu.ph>
2023-02-17 00:18:13 +0800

    php artisan make:model ActualUse --migration --controller --resource

---
+++ app/Http/Controllers/ActualUseController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\ActualUse;
+use Illuminate\Http\Request;
+
+class ActualUseController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\ActualUse  $actualUse
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ActualUse $actualUse)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\ActualUse  $actualUse
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ActualUse $actualUse)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\ActualUse  $actualUse
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ActualUse $actualUse)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\ActualUse  $actualUse
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ActualUse $actualUse)
+    {
+        //
+    }
+}
